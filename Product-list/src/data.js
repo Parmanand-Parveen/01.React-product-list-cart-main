edcbea33512f48ac0d73f0c7db1fc599e0323b8e
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-08-12 01:52:00 +0530

    Tailwind+Data added

---
+++ Product-list/src/data.js
@@ -0,0 +1,66 @@
+const data = [{
+    _id:1,
+    name:'Waffle with Berries',
+    catagory:"Waffle",
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723406991/yza9odvjmwesrqnjmaay.jpg',
+    price:6.50
+    
+},{
+    _id:2,
+    name:'Vanilla Bean Crème Brûlée',
+    catagory:'Crème Brûlée',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407230/qresafbtj6sar8fstfqh.jpg',
+    price: 7.00
+
+}
+,
+{
+    _id:3,
+    name:'Macaron Mix of Five',
+    catagory:'Macaron',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407242/sb81hrlfmdiffntflhjx.jpg',
+    price:8.00
+},
+{
+    _id:4,
+    name:' Classic Tiramisu',
+    catagory:'Tiramisu',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407250/vkklklsjjb4tjxjvn6uw.jpg',
+    price:5.50
+}, 
+{
+    _id:5,
+    name:' Pistachio Baklava',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407261/yad4w0qytmkqffobe5xg.jpg',
+    catagory:'Baklava',
+    price:4.00
+},
+{
+    _id:6,
+    name:'Lemon Meringue Pie',
+    catagory:' Pie',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407270/lnimcez0sdtturhj5t4u.jpg',
+    price: 5.00
+},
+{
+    _id:7,
+    name:'Red Velvet Cake',
+    catagory:'Cake',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407278/oeal3cq5kxhccuucr5bs.jpg',
+    price: 4.50
+},
+{
+    _id:8,
+    name:'Salted Caramel Brownie',
+    catagory:'Brownie',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407293/ddgjyzqxgkzdxgppv0zo.jpg',
+    price: 4.50
+},
+{
+    _id:9,
+    name:'Vanilla Panna Cotta',
+    img:'https://res.cloudinary.com/dwyb76xbb/image/upload/v1723407343/ultd4ucu2cdydgkjmjjk.jpg',
+    catagory:'Panna Cotta',
+    price: 6.50 
+}
+]
